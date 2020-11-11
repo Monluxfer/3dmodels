8d6dca6fb625f1b9261c904e9b70e0402e6d2b7d
Monluxfer <shibarome@gmail.com>
2020-11-11 14:45:03 +0300

    add func

---
+++ Affine/Affine/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Affine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label36 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(660, 681);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 51;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "x + y",
+            "x^2 + y^2",
+            "Sin(x) * Cos(Y)",
+            "Sin(x)",
+            "Cos(Sqrt(x^2 + y^2))",
+            "Sin(Sqrt(x^2 + y^2))"});
+            this.comboBox2.Location = new System.Drawing.Point(737, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(173, 21);
+            this.comboBox2.TabIndex = 112;
+            // 
+            // label36
+            // 
+            this.label36.AutoSize = true;
+            this.label36.Location = new System.Drawing.Point(678, 12);
+            this.label36.Name = "label36";
+            this.label36.Size = new System.Drawing.Size(53, 13);
+            this.label36.TabIndex = 111;
+            this.label36.Text = "Функция";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(737, 39);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(135, 24);
+            this.button8.TabIndex = 110;
+            this.button8.Text = "Построить";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(949, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 113;
+            this.textBox1.Text = "0,2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(916, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 114;
+            this.label1.Text = "Шаг";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(916, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 115;
+            this.label2.Text = "Диапазон";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(984, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 116;
+            this.textBox2.Text = "-6";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(984, 65);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 117;
+            this.textBox3.Text = "6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(919, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 118;
+            this.button1.Text = "Задать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1251, 701);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label36);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label36;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Affine/Affine/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace Affine
+{
+
+
+    public enum Axis { AXIS_X, AXIS_Y, AXIS_Z, LINE };
+    public enum Projection { PERSPECTIVE = 0, ISOMETRIC, ORTHOGR_X, ORTHOGR_Y, ORTHOGR_Z };
+    public enum Clipping { Clipp = 0, ZBuffer, Gouraud, Texture, Graph};
+
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        Polyhedron figure = null;
+        double step = 0.2;
+        int left_range = -6;
+        int right_range = 6;
+
+        Camera camera = new Camera(50,50);
+
+        Color fill_color = Color.Red;
+        byte[] rgbValuesTexture; // for picturebox and texture
+        Bitmap texture;
+        public Bitmap bmp;
+        BitmapData bmpDataTexture; // for picturebox and texture
+        public BitmapData bmpData;
+        public IntPtr ptr; // pointer to the rgbValues
+        public int bytes; // length of rgbValues
+
+        Graphic Graph = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+            g.TranslateTransform(pictureBox1.ClientSize.Width / 2, pictureBox1.ClientSize.Height / 2);
+            g.ScaleTransform(1, -1);
+
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = pictureBox1.CreateGraphics();
+            g.TranslateTransform(pictureBox1.ClientSize.Width / 2, pictureBox1.ClientSize.Height / 2);
+            g.ScaleTransform(1, -1);
+
+
+
+            texture = Image.FromFile("../../texture1.jpg") as Bitmap;
+            Rectangle rectTexture = new Rectangle(0, 0, texture.Width, texture.Height);
+            bmpDataTexture = texture.LockBits(rectTexture, ImageLockMode.ReadWrite, texture.PixelFormat);
+            int bytesTexture = Math.Abs(bmpDataTexture.Stride) * texture.Height;
+            rgbValuesTexture = new byte[bytesTexture];
+            System.Runtime.InteropServices.Marshal.Copy(bmpDataTexture.Scan0, rgbValuesTexture, 0, bytesTexture);
+
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click_1(object sender, EventArgs e)
+        {
+            Graph = new Graphic(comboBox2.SelectedIndex);
+
+            figure = Graph;
+
+            g.Clear(Color.White);
+            Graph.isGraph = true;
+            //Graph.Show(g, 0);
+            Graph.picture = pictureBox1;
+            Graph.DrawGraphic(step, left_range, right_range);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (Graph != null)
+            {
+                if (e.KeyCode == Keys.A)
+                    Graph.psi -= 10;
+                else if (e.KeyCode == Keys.D)
+                    Graph.psi += 10;
+                else if (e.KeyCode == Keys.W)
+                    Graph.phi -= 10;
+                else if (e.KeyCode == Keys.S)
+                    Graph.phi += 10;
+                Graph.DrawGraphic(step, left_range, right_range);
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e) { }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (Graph != null)
+            {
+                Graph.psi += 2;
+                Graph.DrawGraphic(step, left_range, right_range);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            step = double.Parse(textBox1.Text);
+            left_range = int.Parse(textBox2.Text);
+            right_range = int.Parse(textBox3.Text);
+            Graph.DrawGraphic(step, left_range, right_range);
+        }
+    }
+}
